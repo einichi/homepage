441a40dc12b588b4a237b1a9fa2f84a477155ea7
einichi <ricky@burg.in>
2022-06-02 00:03:59 +0900

    escaping quotes

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
         Built with Next.js, Chakra UI + React and more<br />
-        3D Model <Link href="https://skfb.ly/6ZPtS" target="_blank">""My Computer""</Link> by Tobalation is licensed under <Link href="http://creativecommons.org/licenses/by/4.0/" target="_blank">Creative Commons Attribution</Link>.
+        3D Model <Link href="https://skfb.ly/6ZPtS" target="_blank">&quot;My Computer&quot;</Link> by Tobalation is licensed under <Link href="http://creativecommons.org/licenses/by/4.0/" target="_blank">Creative Commons Attribution</Link>.
     </Box>
   )
 }
