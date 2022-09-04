e0e1805ac9a20b718d5d6d35548a24d73bdfc704
einichi <ricky@burg.in>
2022-09-04 16:00:25 +0900

    adding favico

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,22 @@ export default class Document extends NextDocument {
     render () {
         return(
             <Html lang="en">
-                <Head />
-                <body>
-                    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                    <Main />
-                    <NextScript />
-                </body>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon/favicon-16x16.png" />
+                <link rel="manifest" href="/public/favicon/site.webmanifest" />
+                <link rel="mask-icon" href="/public/favicon/safari-pinned-tab.svg" color="#000000" />
+                <link rel="shortcut icon" href="/public/favicon/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="msapplication-config" content="/public/favicon/browserconfig.xml" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+            <body>
+                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                <Main />
+                <NextScript />
+            </body>
             </Html>
         )
     }
