dd3fcfc5dd253f332e0ed0636ed8e284319f8458
einichi <ricky@burg.in>
2023-02-06 13:01:31 +0900

    removed ref to previous 3D model

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,9 @@
-import { Box, Link } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      Built with Next.js, Chakra UI + React and more
-      <br />
-      3D Model{' '}
-      <Link href="https://skfb.ly/6ZPtS" target="_blank">
-        &quot;My Computer&quot;
-      </Link>{' '}
-      by Tobalation is licensed under{' '}
-      <Link href="http://creativecommons.org/licenses/by/4.0/" target="_blank">
-        Creative Commons Attribution
-      </Link>
-      .
+      Built with Next.js, Chakra UI + React, Three.JS and more
     </Box>
   )
 }
