dbf9836abfa41f04cffeb0cf2f244bc4589ee37f
einichi <ricky@burg.in>
2023-02-06 13:54:25 +0900

    update options

--- libs/model.js
+++ libs/model.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 export function loadGLTFModel(
   scene,
   glbPath,
-  options = { receiveShadow: true, castShadow: true }
+  options = { receiveShadow: false, castShadow: false }
 ) {
   const { receiveShadow, castShadow } = options
   return new Promise((resolve, reject) => {
